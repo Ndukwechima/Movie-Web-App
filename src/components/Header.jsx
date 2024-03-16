a058d06caeb43bf1d2cdbe066c47d36414ddbc17
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-03-16 21:41:47 +0100

    add dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,14 @@ import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
+
+// API_Key = 981512264034990ace65fe84a822b7c5
+// ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODE1MTIyNjQwMzQ5OTBhY2U2NWZlODRhODIyY
+// jdjNSIsInN1YiI6IjY1ZjJlNGRhMmZkZWM2MDE1NzI
+// wZGQ3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJza
+// W9uIjoxfQ.UhjrPTukwHqK9wB
+// -i30VZcUPtcqzvfN5nmVdd_ynQfY";
 
 function Header() {
   return (
@@ -11,12 +19,15 @@ function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
-          IMDB
-        </span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDB
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
