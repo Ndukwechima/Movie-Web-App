43210d96a5b47ab7cb055ee113916e32180a97dc
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-03-16 22:01:58 +0100

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque a
+        laudantium mollitia recusandae eos, ut fugiat laborum quas nihil omnis
+        Praesentium nostrum amet facilis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat modi
+        eum expedita iusto? Soluta accusantium temporibus eaque sint atque,
+        sequi natus dolor aliquam, recusandae corrupti nobis obcaecati impedit,
+        laborum aliquid vel! Consequuntur, voluptatem maxime excepturi sed sunt
+        minima commodi illum minus ullam odio repellat maiores incidunt saepe,
+        doloribus rem nemo, dignissimos ipsam a error nobis. Possimus repellat
+        laborum quas nihil omnis molestias eaque cupiditate obcaecati, repellat
+        accusamus nobis debitis laborum quas nihil omnis molestias eaque
+        cupiditate obcaecati, repellat accusamus nobis debitis temporibus soluta
+        fuga maiores temporibus soluta fuga maiores eveniet dolorem est?
+      </p>
+    </div>
+  );
+}
+
+export default About;
